50207a8eee965c1423a1d4586e67f05981d8848f
Alex H <alex.holder@bigfootds.com>
2023-10-21 11:12:23 +1100

    fix: test uses variable for DRYness

--- test/server.test.js
+++ test/server.test.js
@@ -18,14 +18,17 @@ describe("Server root route exists and returns hello world", () => {
 });
 
 describe("POST to root route copies message in request body", () => {
-	test('POST request.body.message of "Hello world" returns received value of "Hello world"', async () => { 
+	test('POST request.body.message of "Hello world" returns received value of "Hello world"', async () => {
+		
+		let messageToSend = "Hello world";
+
 		const response = await request(app)
 		.post("/")
 		.send({
-			message: "Hello world"
+			message: messageToSend
 		});
 
-		expect(response.body.received).toEqual("Hello world");
+		expect(response.body.received).toEqual(messageToSend);
 
 
 	});
