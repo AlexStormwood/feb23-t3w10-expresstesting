a97d112fe6511ceb82cbdaaafc97ccdf51bfd515
Alex H <alex.holder@bigfootds.com>
2023-10-21 11:06:42 +1100

    feat: test with post data implemented

--- test/server.test.js
+++ test/server.test.js
@@ -16,3 +16,17 @@ describe("Server root route exists and returns hello world", () => {
 		expect(response.body.message).toEqual("Hello world!");
 	});
 });
+
+describe("POST to root route copies message in request body", () => {
+	test('POST request.body.message of "Hello world" returns received value of "Hello world"', async () => { 
+		const response = await request(app)
+		.post("/")
+		.send({
+			message: "Hello world"
+		});
+
+		expect(response.body.received).toEqual("Hello world");
+
+
+	});
+});
