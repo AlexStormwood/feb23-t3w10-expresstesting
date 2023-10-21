585ba1c0c1b6eb4c2776ec15d8806aee3c648f22
Alex H <alex.holder@bigfootds.com>
2023-10-21 11:00:54 +1100

    feat: post request implemented

--- src/server.js
+++ src/server.js
@@ -8,9 +8,28 @@ const PORT = process.env.PORT || 3000;
 // Sometimes WSL is quirky, needs this: 
 // const HOST = '0.0.0.0';
 
+// This lets us read POST'd JSON body data on the request 
+app.use(express.json());
+
+// ^
+// |
+// Configuration goes above
+// Routes go below 
+// |
+// v
+
+
 app.get("/", (request, response) => {
 	response.json({
 		message:"Hello world!"
+	});
+});
+
+app.post("/", (request, response) => {
+	// Just copy what the posted data is and 
+	// attach it to response.body.received 
+	response.json({
+		received: request.body.message
 	});
 });
 
